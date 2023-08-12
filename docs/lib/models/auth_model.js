1824b9ea136c98bfe501920d8afc1de677125c99
KolibSoft <kolibsoftware@gmail.com>
2023-08-12 11:33:50 -0600

    Spell fixing

--- docs/lib/models/auth_model.js
+++ docs/lib/models/auth_model.js
@@ -13,13 +13,13 @@ class AuthModel {
     accessToken;
 
     /** @type {string} */
-    rerfreshToken;
+    refreshToken;
 
     constructor(json) {
         this.credential = json?.credential ?? new CredentialModel({ id: "00000000-0000-0000-0000-000000000000" });
         this.permissions = json?.permissions ?? [];
         this.accessToken = json?.accessToken ?? "";
-        this.rerfreshToken = json?.rerfreshToken ?? "";
+        this.refreshToken = json?.refreshToken ?? "";
     }
 
 }
